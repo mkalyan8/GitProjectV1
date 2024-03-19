339a38cdc7e2bd63ed5ee217a7a311bdc2903302
AIN166 <naveen81814@gmail.com>
2024-03-19 16:15:38 +0530

    Added additional checks

--- src/Test2.java
+++ src/Test2.java
@@ -2,7 +2,7 @@ public class Test2
 {
     public static void main(String[] args) {
         String hungry = "Yes";
-       if (hungry == "Yes")
+       if (hungry == "Yes" || hungry == "Y" || hungry == "y")
        {
            System.out.println("Eat something........          ");
        }
